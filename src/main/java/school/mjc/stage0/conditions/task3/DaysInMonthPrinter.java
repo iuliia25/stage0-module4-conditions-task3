bafab45fa3ef9677c170c983d58ebf03f881e81f
iuliia25 <khlystun.iuliia@gmail.com>
2022-09-06 21:07:52 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
-        if (month < 1 && month > 12) {
+        if (month < 1 || month > 12) {
             System.out.println("wrong number!");
         } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
             System.out.println(31);
