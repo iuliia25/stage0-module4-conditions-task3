18ad11b0d4364ec8e05eca5cb0fa301cec8b562b
iuliia25 <khlystun.iuliia@gmail.com>
2022-09-07 17:44:29 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -4,10 +4,10 @@ public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
         String vowels = "aeiouyAEIOUY";
         String consonant = "qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNM";
-        Character a = new Character(character);
-        if (vowels.contains (a)) {
+        Character a = Character.valueOf(character);
+        if (vowels.contains (a.toString())) {
             System.out.println("Vowel");
-        } else if (consonant.contains(a)) {
+        } else if (consonant.contains(a.toString())) {
             System.out.println("Consonant");
         } else  {
             System.out.println("wrong alphabet!");
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task3;
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
         String eng = "QWERTYUIOPASDFGHJKLZXCVBNMqwrtyuiopasdfghjklzxcvnbnm";
-        Character a= new Character(symbol);
+        Character a= Character.valueOf(symbol);
         if (eng.contains (a.toString())) {
             System.out.println("English");
         } else {
