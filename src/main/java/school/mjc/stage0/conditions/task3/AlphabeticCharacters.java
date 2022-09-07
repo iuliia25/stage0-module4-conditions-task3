3d4bd9e88ff8ace966e8a67ebd1434c7820cee7d
iuliia25 <khlystun.iuliia@gmail.com>
2022-09-07 17:34:48 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -4,10 +4,10 @@ public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
         String vowels = "aeiouyAEIOUY";
         String consonant = "qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNM";
-        String s = new String(character);
-        if (vowels.contains (s)) {
+        Character a = new Character(character);
+        if (vowels.contains (a)) {
             System.out.println("Vowel");
-        } else if (consonant.contains(s)) {
+        } else if (consonant.contains(a)) {
             System.out.println("Consonant");
         } else  {
             System.out.println("wrong alphabet!");
